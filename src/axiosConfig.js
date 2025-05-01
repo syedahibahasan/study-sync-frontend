f8fb6d2bdfe152ba3eaef7684251252100c1bc6c
syedahibahasan <hibahasan151214@gmail.com>
2025-05-01 04:11:09 -0700

    Fix: Use REACT_APP_API_URL properly in axios config

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NODE_ENV !== "production"
-    ? "http://localhost:5001"
-    : process.env.REACT_APP_API_URL;
+// Always prefer the environment variable if it's defined
+const baseURL = process.env.REACT_APP_API_URL || "http://localhost:5001";
 
 axios.defaults.baseURL = baseURL;
